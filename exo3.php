07a6b73699a9be9a63a14e0ab4e412320f9e7294
DavidV <davidv@DavidVs-MacBook-Pro.local>
2017-09-11 13:01:06 +0200

    fin exo 3

--- exo3.php
+++ exo3.php
@@ -1,26 +1,96 @@
 <?php
+session_start();
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
 class DataBase {
-	function __constructor($username, $password, $DBname, $localhost){
-		$this->username = $username;
-		$this->password = $password;
-		$this->connection = new PDO('mysql:host=$localhost;dbname=$DBname', $username, $password);
-	}
 
-	function query(requete){
-		$this->connection->query(requete);
-
+	public static function bdd(){
+		$host = 'localhost';
+		$DBname = 'pooDB';
+		$username = 'root';
+		$password = 'root';
+		try {
+		 	return new PDO('mysql:host='.$host.';dbname='.$DBname, $username, $password);
+		} catch (PDOException $e){
+			print "Erreur : " . $e->getMessage() ."</br>";
+		}
 	}
 }
 
 class User{
-	function __constructor($id, $username, $email, $password){
-		$this->id = $id;
+	public $id;
+	public $username;
+	public $email;
+	public $password;
+
+	function __construct($username, $email, $password){
 		$this->username = $username;
 		$this->email = $email;
 		$this->password = $password;
 	}
 
+	public function request($query){
+		$connected = DataBase::bdd();
+		$prepared = $connected->prepare($query);
+		$prepared->execute();
+		$data = $prepared->fetchAll();
+		return $data;
+	}
 
+	public function create(){
+		$query = "SELECT * FROM users WHERE username = '$this->username' ";
+		$connected = DataBase::bdd();
+		$prepared = $connected->prepare($query);
+		$prepared->execute();
+		$data = $prepared->fetchAll();
+		if (!$data[0]){
+			$query = "INSERT INTO users(username, email, password) VALUES('$this->username', '$this->email', '$this->password')";
+			$connected = DataBase::bdd();
+			$prepared = $connected->prepare($query);
+			$prepared->execute();
+		} else{
+			echo "L'username est déjà connu en DB";
+		}
+
+	}
+
+	public function sessionConnect(){
+		$query = "SELECT ID FROM users WHERE username = '$this->username' ";
+		$connected = DataBase::bdd();
+		$prepared = $connected->prepare($query);
+		$prepared->execute();
+		$data = $prepared->fetchAll();
+		$_SESSION['ID'] = $data[0]['ID'];
+	}
+
+	public function sessionDeconnect(){
+		session_destroy();
+	}
+
+	public function nameUpdate($newUsername){
+		$this->username = $newUsername;
+		$query = "UPDATE users SET username = '$this->username' WHERE email = '$this->email' ";
+		$connected = DataBase::bdd();
+		$prepared = $connected->prepare($query);
+		$prepared->execute();
+	}
+
+	public function emailUpdate($newEmail){
+		$this->email = $newEmail;
+		$query = "UPDATE users SET email = '$this->email' WHERE username = '$this->username' ";
+		$connected = DataBase::bdd();
+		$prepared = $connected->prepare($query);
+		$prepared->execute();
+	}
+
+	public function delete(){
+		$query = "DELETE FROM users WHERE username = '$this->username' ";
+		$connected = DataBase::bdd();
+		$prepared = $connected->prepare($query);
+		$prepared->execute();
+	}
 }
 
 ?>
@@ -33,9 +103,11 @@ class User{
 </head>
 <body>
 <?php
-	$DB = new DataBase('root', 'root');
-	$DB->connection->query('SELECT * FROM users');
 
+	echo 'testy </br>';
+	$perso = new User('david', 'vdv.david@gmail.com', 'root');
+	$perso->create();
+	$perso->sessionConnect();
 
 
 ?>
